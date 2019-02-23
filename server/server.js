fd96afc01da2afcfbdf8262f7f1eb4b50ef6ef97
Preet Dabre <dabre.preet@gmail.com>
2019-02-23 12:18:10 +0530

    Added basic chat feature via jQuery

--- server/server.js
+++ server/server.js
@@ -23,10 +23,10 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User joined'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('Create Message', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
-
+        callback('This is from server');
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
